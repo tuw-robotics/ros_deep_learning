34b4de0f0acdf3951f6fe9359dc141f00cafa487
Florian Beck <florian.beck@student.tuwien.ac.at>
2017-09-12 13:29:08 +0200

    added ground plane subscriber

--- src/ros_detectnet.cpp
+++ src/ros_detectnet.cpp
@@ -1,227 +1,231 @@
-#include <ros/ros.h>
-#include <jetson-inference/detectNet.h>
+#include "ros_detectnet.h"
 
-#include <jetson-inference/loadImage.h>
-#include <jetson-inference/cudaFont.h>
-
-#include <jetson-inference/cudaMappedMemory.h>
-#include <jetson-inference/cudaNormalize.h>
-
-#include <opencv2/core.hpp>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
-#include <std_msgs/String.h>
-#include <std_msgs/Int32.h>
-
-#include <pluginlib/class_list_macros.h>
-#include <nodelet/nodelet.h>
-
-namespace ros_deep_learning{
-
-class ros_detectnet : public nodelet::Nodelet
+namespace ros_deep_learning
 {
-    public:
-        ~ros_detectnet()
-        {
-            ROS_INFO("\nshutting down...\n");
-            if(gpu_data)
-                CUDA(cudaFree(gpu_data));
-            delete net;
-        }
-        void onInit()
-        {
-            // get a private nodehandle
-            ros::NodeHandle& private_nh = getPrivateNodeHandle();
-            // get parameters from server, checking for errors as it goes
-            
-            std::string prototxt_path, model_path, mean_binary_path, class_labels_path;
-            if(! private_nh.getParam("prototxt_path", prototxt_path) )
-                ROS_ERROR("unable to read prototxt_path for detectnet node");
-            if(! private_nh.getParam("model_path", model_path) )
-                ROS_ERROR("unable to read model_path for detectnet node");
+ros_detectnet::~ros_detectnet()
+{
+  ROS_INFO("\nshutting down...\n");
+  if (gpu_data_)
+    CUDA(cudaFree(gpu_data_));
+  delete net_;
+}
+void ros_detectnet::onInit()
+{
+  // get a private nodehandle
+  ros::NodeHandle& private_nh = getPrivateNodeHandle();
+  // get parameters from server, checking for errors as it goes
 
-            // make sure files exist (and we can read them)
-            if( access(prototxt_path.c_str(),R_OK) )
-                 ROS_ERROR("unable to read file \"%s\", check filename and permissions",prototxt_path.c_str());
-            if( access(model_path.c_str(),R_OK) )
-                 ROS_ERROR("unable to read file \"%s\", check filename and permissions",model_path.c_str());
+  std::string prototxt_path, model_path, mean_binary_path, class_labels_path;
+  if (!private_nh.getParam("prototxt_path", prototxt_path))
+    ROS_ERROR("unable to read prototxt_path for detectnet node");
+  if (!private_nh.getParam("model_path", model_path))
+    ROS_ERROR("unable to read model_path for detectnet node");
 
-            // create imageNet
-            //net = imageNet::Create(prototxt_path.c_str(),model_path.c_str(),NULL,class_labels_path.c_str());
-            
-            
+  // make sure files exist (and we can read them)
+  if (access(prototxt_path.c_str(), R_OK))
+    ROS_ERROR("unable to read file \"%s\", check filename and permissions", prototxt_path.c_str());
+  if (access(model_path.c_str(), R_OK))
+    ROS_ERROR("unable to read file \"%s\", check filename and permissions", model_path.c_str());
 
-            // create detectNet
-            //net = detectNet::Create(detectNet::PEDNET, 0.5f, 2);
-            net = detectNet::Create(prototxt_path.c_str(), model_path.c_str(), 0.0f, 0.5f, DETECTNET_DEFAULT_INPUT, DETECTNET_DEFAULT_COVERAGE, DETECTNET_DEFAULT_BBOX, 2);
+  // create imageNet
+  // net = imageNet::Create(prototxt_path.c_str(),model_path.c_str(),NULL,class_labels_path.c_str());
 
-            if( !net )
-            {
-                ROS_INFO("detectnet-console:   failed to initialize detectNet\n");
-                return;
-            }
+  // create detectNet
+  // net = detectNet::Create(detectNet::PEDNET, 0.5f, 2);
+  net_ = detectNet::Create(prototxt_path.c_str(), model_path.c_str(), 0.0f, 0.5f, DETECTNET_DEFAULT_INPUT,
+                           DETECTNET_DEFAULT_COVERAGE, DETECTNET_DEFAULT_BBOX, 2);
 
-            // setup image transport
-            image_transport::ImageTransport it(private_nh);
-            // subscriber for passing in images
-            imsub = it.subscribe("imin", 10, &ros_detectnet::callback, this);
-            
-            impub = it.advertise("image_out", 1);
-            
-            // publisher for classifier output
-            //class_pub = private_nh.advertise<std_msgs::Int32>("class",10);
-            // publisher for human-readable classifier output
-            //class_str_pub = private_nh.advertise<std_msgs::String>("class_str",10);
+  if (!net_)
+  {
+    ROS_INFO("ros_detectnet: failed to initialize detectNet\n");
+    return;
+  }
 
-            // init gpu memory
-            gpu_data = NULL;
-        }
+  // setup image transport
+  image_transport::ImageTransport it(private_nh);
+  // subscriber for passing in images
+  camsub_ = it.subscribeCamera("imin", 10, &ros_detectnet::cameraCallback, this);
 
+  impub_ = it.advertise("image_out", 1);
 
-    private:
-        void callback(const sensor_msgs::ImageConstPtr& input)
-        {
-            cv::Mat cv_im = cv_bridge::toCvCopy(input, "bgr8")->image;
-            cv::Mat cv_result;
-            
-            //impub.publish(cv_bridge::CvImage(std_msgs::Header(), "bgr8", cv_im).toImageMsg());
-            
-            ROS_INFO("image ptr at %p",cv_im.data);
-            // convert bit depth
-            cv_im.convertTo(cv_im,CV_32FC3);
-            // convert color
-            cv::cvtColor(cv_im,cv_im,CV_BGR2RGBA);
+  personpub_ = private_nh.advertise<tuw_object_msgs::ObjectDetection>("detected_persons_tuw", 1);
 
-            // allocate GPU data if necessary
-            if(gpu_data == NULL){
-                ROS_INFO("first allocation");
-                CUDA(cudaMalloc(&gpu_data, cv_im.rows*cv_im.cols * sizeof(float4)));
-            }else if(imgHeight != cv_im.rows || imgWidth != cv_im.cols){
-                ROS_INFO("re allocation");
-                // reallocate for a new image size if necessary
-                CUDA(cudaFree(gpu_data));
-                CUDA(cudaMalloc(&gpu_data, cv_im.rows*cv_im.cols * sizeof(float4)));
-            }
-            
-            /*
-            * allocate memory for output bounding boxes and class confidence
-            */
-            const uint32_t maxBoxes = net->GetMaxBoundingBoxes();		printf("maximum bounding boxes:  %u\n", maxBoxes);
-            const uint32_t classes  = net->GetNumClasses();
-            
-            float* bbCPU    = NULL;
-            float* bbCUDA   = NULL;
-            float* confCPU  = NULL;
-            float* confCUDA = NULL;
-            
-            if( !cudaAllocMapped((void**)&bbCPU, (void**)&bbCUDA, maxBoxes * sizeof(float4)) ||
-                !cudaAllocMapped((void**)&confCPU, (void**)&confCUDA, maxBoxes * classes * sizeof(float)) )
-            {
-              printf("detectnet-console:  failed to alloc output memory\n");
-            }
+  // publisher for classifier output
+  // class_pub = private_nh.advertise<std_msgs::Int32>("class",10);
+  // publisher for human-readable classifier output
+  // class_str_pub = private_nh.advertise<std_msgs::String>("class_str",10);
 
-            int numBoundingBoxes = maxBoxes;
-            
-            imgHeight = cv_im.rows;
-            imgWidth = cv_im.cols;
-            imgSize = cv_im.rows*cv_im.cols * sizeof(float4);
-            float4* cpu_data = (float4*)(cv_im.data);
+  // init gpu memory
+  gpu_data_ = NULL;
+}
 
-            // copy to device
-            CUDA(cudaMemcpy(gpu_data, cpu_data, imgSize, cudaMemcpyHostToDevice));
+void ros_detectnet::cameraCallback(const sensor_msgs::ImageConstPtr& input,
+                                   const sensor_msgs::CameraInfoConstPtr& camera_info)
+{
+  // camera matrix
+  Eigen::Matrix<double, 3, 3, Eigen::RowMajor> K(camera_info->K.data());
+  Eigen::Matrix<double, 3, 3, Eigen::RowMajor> K_inv = K.inverse();
 
-            bool det_result = net->Detect((float*)gpu_data, imgWidth, imgHeight, bbCPU, &numBoundingBoxes, confCPU);
-            
-            if(det_result)
-            {
-              int lastClass = 0;
-              int lastStart = 0;
-              
-              for( int n=0; n < numBoundingBoxes; n++ )
-              {
-                const int nc = confCPU[n*2+1];
-                float* bb = bbCPU + (n * 4);
-                
-                printf("bounding box %i   (%f, %f)  (%f, %f)  w=%f  h=%f\n", n, bb[0], bb[1], bb[2], bb[3], bb[2] - bb[0], bb[3] - bb[1]); 
-                
-                if( nc != lastClass || n == (numBoundingBoxes - 1) )
-                {                  
-                  
-                  if( !net->DrawBoxes((float*)gpu_data, (float*)gpu_data, imgWidth, imgHeight, 
-                                            bbCUDA + (lastStart * 4), (n - lastStart) + 1, lastClass) )
-                    printf("detectnet-console:  failed to draw boxes\n");
-                    
-                  
-                  //CUDA(cudaNormalizeRGBA((float4*)gpu_data, make_float2(0.0f, 255.0f), (float4*)gpu_data, make_float2(0.0f, 1.0f), imgWidth, imgHeight));
-                  
-                  // back to host
-                  CUDA(cudaMemcpy(cpu_data, gpu_data, imgSize, cudaMemcpyDeviceToHost));
-                  
-                  lastClass = nc;
-                  lastStart = n;
-                  
-                  /*
-                  cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE);
-                  cv::imshow("Display window", cv::Mat(imgHeight, imgWidth, CV_8UC4, cpu_data));
-                  cv::waitKey(0);
-                  */
+  cv::Mat cv_im = cv_bridge::toCvCopy(input, "bgr8")->image;
+  cv::Mat cv_result;
 
-                  CUDA(cudaDeviceSynchronize());
-                }
-              }
-            }
-            else
-            {
-              std::cout << "detectnet: detection error occured" << std::endl;
-            }
-            
-            //float confidence = 0.0f;
+  ROS_DEBUG("ros_detectnet: image ptr at %p", cv_im.data);
+  // convert bit depth
+  cv_im.convertTo(cv_im, CV_32FC3);
+  // convert color
+  cv::cvtColor(cv_im, cv_im, CV_BGR2RGBA);
 
-            // classify image
-            //const int img_class = net->Classify((float*)gpu_data, imgWidth, imgHeight, &confidence);
+  // allocate GPU data if necessary
+  if (gpu_data_ == NULL)
+  {
+    ROS_DEBUG("ros_detectnet: first allocation");
+    CUDA(cudaMalloc(&gpu_data_, cv_im.rows * cv_im.cols * sizeof(float4)));
+  }
+  else if (imgHeight_ != cv_im.rows || imgWidth_ != cv_im.cols)
+  {
+    ROS_DEBUG("ros_detectnet: re allocation");
+    // reallocate for a new image size if necessary
+    CUDA(cudaFree(gpu_data_));
+    CUDA(cudaMalloc(&gpu_data_, cv_im.rows * cv_im.cols * sizeof(float4)));
+  }
 
-            // publish class 
-            /*
-            std_msgs::Int32 class_msg;
-            class_msg.data = img_class;
-            class_pub.publish(class_msg);
-            // publish class string
-            std_msgs::String class_msg_str;
-            class_msg_str.data = net->GetClassDesc(img_class);
-            class_str_pub.publish(class_msg_str);
+  // allocate memory for output bounding boxes
+  const uint32_t maxBoxes = net_->GetMaxBoundingBoxes();
+  ROS_DEBUG("ros_detectnet: maximum bounding boxes: %u\n", maxBoxes);
+  const uint32_t classes = net_->GetNumClasses();
 
-            if( img_class < 0 )
-                ROS_INFO("imagenet-console:  failed to classify (result=%i)\n", img_class);
+  float* bbCPU = NULL;
+  float* bbCUDA = NULL;
+  float* confCPU = NULL;
+  float* confCUDA = NULL;
 
-            */
-            
-            cv_result = cv::Mat(imgHeight, imgWidth, CV_32FC4, cpu_data);
-            cv_result.convertTo(cv_result, CV_8UC4);
-            
-            cv::cvtColor(cv_result, cv_result, CV_RGBA2BGR);
-                  
-            impub.publish(cv_bridge::CvImage(std_msgs::Header(), "bgr8", cv_result).toImageMsg());
-        }
+  if (!cudaAllocMapped((void**)&bbCPU, (void**)&bbCUDA, maxBoxes * sizeof(float4)) ||
+      !cudaAllocMapped((void**)&confCPU, (void**)&confCUDA, maxBoxes * classes * sizeof(float)))
+  {
+    ROS_ERROR("ros_detectnet: failed to alloc output memory\n");
+  }
 
-        // private variables
-        image_transport::Subscriber imsub;
-        image_transport::Publisher impub;
-        
-        //ros::Publisher class_pub;
-        //ros::Publisher class_str_pub;
-        detectNet* net;
+  int numBoundingBoxes = maxBoxes;
 
-        float4* gpu_data;
+  imgHeight_ = cv_im.rows;
+  imgWidth_ = cv_im.cols;
+  imgSize_ = cv_im.rows * cv_im.cols * sizeof(float4);
+  float4* cpu_data = (float4*)(cv_im.data);
 
-        uint32_t imgWidth;
-        uint32_t imgHeight;
-        size_t   imgSize;
-};
+  std::vector<Eigen::Vector3f> center_points;
 
-PLUGINLIB_DECLARE_CLASS(ros_deep_learning, ros_detectnet, ros_deep_learning::ros_detectnet, nodelet::Nodelet);
+  // copy to device
+  CUDA(cudaMemcpy(gpu_data_, cpu_data, imgSize_, cudaMemcpyHostToDevice));
 
-} // namespace ros_deep_learning
+  bool det_result = net_->Detect((float*)gpu_data_, imgWidth_, imgHeight_, bbCPU, &numBoundingBoxes, confCPU);
+
+  if (det_result)
+  {
+    int lastClass = 0;
+    int lastStart = 0;
+
+    for (int n = 0; n < numBoundingBoxes; n++)
+    {
+      const int nc = confCPU[n * 2 + 1];
+      float* bb = bbCPU + (n * 4);
+
+      ROS_INFO("ros_detectnet: bounding box %i   (%f, %f)  (%f, %f)  w=%f  h=%f\n", n, bb[0], bb[1], bb[2], bb[3],
+               bb[2] - bb[0], bb[3] - bb[1]);
+
+      if (nc != lastClass || n == (numBoundingBoxes - 1))
+      {
+        if (!net_->DrawBoxes((float*)gpu_data_, (float*)gpu_data_, imgWidth_, imgHeight_, bbCUDA + (lastStart * 4),
+                             (n - lastStart) + 1, lastClass))
+          ROS_ERROR("ros_detectnet: failed to draw boxes\n");
+
+        // calculate ground center of bounding box
+        Eigen::Vector3f P1_img;
+        P1_img(0) = bb[0] + (bb[2] - bb[0]) / 2;
+        P1_img(1) = bb[1];
+        P1_img(2) = 1;
+        center_points.emplace_back(P1_img);
+
+        // CUDA(cudaNormalizeRGBA((float4*)gpu_data_, make_float2(0.0f, 255.0f), (float4*)gpu_data_, make_float2(0.0f,
+        // 1.0f), imgWidth_, imgHeight_));
+
+        // copy back to host
+        CUDA(cudaMemcpy(cpu_data, gpu_data_, imgSize_, cudaMemcpyDeviceToHost));
+
+        lastClass = nc;
+        lastStart = n;
+
+        CUDA(cudaDeviceSynchronize());
+      }
+    }
+  }
+  else
+  {
+    ROS_ERROR("detectnet: detection error occured");
+  }
+
+  Eigen::Vector3f P0(0, 0, 0);
+  Eigen::Vector3f P1;
+  Eigen::Vector3f P_diff;
+  Eigen::Vector3f P3D;
+
+  cv_result = cv::Mat(imgHeight_, imgWidth_, CV_32FC4, cpu_data);
+  cv_result.convertTo(cv_result, CV_8UC4);
+
+  cv::cvtColor(cv_result, cv_result, CV_RGBA2BGR);
+
+  tuw_object_msgs::ObjectDetection detected_persons_tuw;
+  detected_persons_tuw.header = input->header;
+
+  for (size_t i = 0; i < center_points.size(); i++)
+  {
+    cv::circle(cv_result, cv::Point(center_points[i](0), center_points[i](1)), 2, cv::Scalar(0, 0, 255), -1, 8, 0);
+
+    // calculate 3D position through intersection with ground plane
+    P1 = center_points[i];
+    P_diff = P1 - P0;
+    float nom = gpd_ - gpn_.dot(P0);
+    float denom = gpn_.dot(P_diff);
+    P3D = P0 + nom / denom * P_diff;
+
+    tuw_object_msgs::ObjectWithCovariance obj;
+    obj.covariance_pose.emplace_back(0.5);
+    obj.covariance_pose.emplace_back(0);
+    obj.covariance_pose.emplace_back(0);
+    obj.covariance_pose.emplace_back(0);
+    obj.covariance_pose.emplace_back(0.5);
+    obj.covariance_pose.emplace_back(0);
+    obj.covariance_pose.emplace_back(0);
+    obj.covariance_pose.emplace_back(0);
+    obj.covariance_pose.emplace_back(0);
+
+    obj.object.ids.emplace_back(i);
+    obj.object.ids_confidence.emplace_back(1.0);
+    obj.object.pose.position.x = -P3D(0);
+    obj.object.pose.position.y = -P3D(1);
+    obj.object.pose.position.z = -P3D(2);
+    obj.object.pose.orientation.x = 0.0;
+    obj.object.pose.orientation.y = 0.0;
+    obj.object.pose.orientation.z = 0.0;
+    obj.object.pose.orientation.w = 1.0;
+
+    detected_persons_tuw.objects.emplace_back(obj);
+  }
+
+  personpub_.publish(detected_persons_tuw);
+  impub_.publish(cv_bridge::CvImage(std_msgs::Header(), "bgr8", cv_result).toImageMsg());
+}
+
+void ros_detectnet::groundPlaneCallback(const rwth_perception_people_msgs::GroundPlane::ConstPtr& gp)
+{
+  gp_ = gp;
+
+  // ground plane normal vector
+  gpn_(0) = gp_->n[0];
+  gpn_(1) = gp_->n[1];
+  gpn_(2) = gp_->n[2];
+
+  // ground plane distance ax+by+cz = d
+  double gpd_ = ((double)gp_->d) * (-1000.0);
+}
+
+}  // namespace ros_deep_learning
